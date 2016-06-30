505459e69a52d3e7bfbf9a462c3591adc56ad5b8
harapannias <niasharapan@gmail.com>
2016-06-30 14:54:50 +0700

    Mengupdate welcome.blade.php

--- private/resources/views/welcome.blade.php
+++ private/resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Welkam tu Peredais</div>
+                <div class="title">Welkam tu Peredais Update from svn</div>
             </div>
         </div>
     </body>
