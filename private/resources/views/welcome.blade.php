24e8be1868f11665e18d83100d2a2913e71b4514
harapannias <niasharapan@gmail.com>
2016-06-30 14:48:40 +0700

    Pesannya Apa pake tanda kutip

--- private/resources/views/welcome.blade.php
+++ private/resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">SELAMAT DATANG</div>
+                <div class="title">Welkam tu Peredais</div>
             </div>
         </div>
     </body>
